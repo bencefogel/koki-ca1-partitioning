b7aced13f62d57c5a43a81a9a252db64e47e18c1
fogel.bence <fogel.bence@hallgato.itk.ppke.hugit config --global --listlsgit remote addgit config --global user.email fogel.bence@hallgato.itk.ppke.hu>
2024-12-20 11:04:40 +0100

    Add function to partition axial currents across multiple timepoints.

--- partitioning_algorithm.py
+++ partitioning_algorithm.py
@@ -3,11 +3,57 @@ import numpy as np
 from pandas import DataFrame, Series
 
 
-def partition_iax(ref: str, par: str, tp: int, df_im: DataFrame, df_iax: DataFrame) -> None:
-    im_tp = df_im.loc[ref, tp]
+def part_iax_multiple(im: DataFrame, iax: DataFrame, timepoints: list, target: str) -> DataFrame:
+    """
+    Partitions axial currents into membrane currents across multiple time points and updates a copy of the membrane currents DataFrame.
+
+    Parameters:
+        im (DataFrame): A DataFrame containing membrane currents for each node and time point.
+        iax (DataFrame): A DataFrame containing axial currents for each reference-parent pair and time point.
+        timepoints (list): A list of time points for which the partitioning process is performed.
+        target (str): The target node to start the partitioning traversal from.
+
+    Returns:
+        DataFrame: A modified copy of `im` with updated membrane currents after partitioning axial currents for all time points.
+    """
+    im_part = im.copy()
+
+    for tp in tqdm(timepoints):
+        dg = create_directed_graph(iax, tp)
+
+        partitioning_order_out = get_partitioning_order(dg, target, 'out')
+        for segment_pair in partitioning_order_out:
+            ref = segment_pair[0]
+            par = segment_pair[1]
+            partition_iax(ref, par, tp, im_part, iax)
+
+        partitioning_order_in = get_partitioning_order(dg, target, 'in')
+        for segment_pair in partitioning_order_in:
+            ref = segment_pair[0]
+            par = segment_pair[1]
+            partition_iax(ref, par, tp, im_part, iax)
+
+    return im_part
+
+
+def partition_iax(ref: str, par: str, tp: int, im: DataFrame, iax: DataFrame) -> None:
+    """
+    Partitions axial currents at a specific time point into membrane currents and updates the parent node's membrane currents.
+
+    Parameters:
+        ref (str): The reference node (child node) in the current partitioning process.
+        par (str): The parent node in the current partitioning process.
+        tp (int): The time point for which the partitioning is performed.
+        im (DataFrame): A DataFrame containing membrane currents for each node and time point.
+        iax (DataFrame): A DataFrame containing axial currents for each reference-parent pair and time point.
+
+    Returns:
+        None: The function updates the `df_im` DataFrame in place with the partitioned axial currents added to the parent node's membrane currents.
+    """
+    im_tp = im.loc[ref, tp]
     im_tp_out = im_tp[im_tp >= 0]
     im_tp_in = im_tp[im_tp < 0]
-    iax_tp = df_iax.loc[(ref, par), tp]
+    iax_tp = iax.loc[(ref, par), tp]
 
     # Calculate partitioned membrane currents
     if iax_tp >= 0:
@@ -16,12 +62,22 @@ def partition_iax(ref: str, par: str, tp: int, df_im: DataFrame, df_iax: DataFra
         part_curr = get_part_in(iax_tp, im_tp_in)
 
     # Update membrane currents of the parent with the partitioned axial current values
-    part_curr = part_curr.reindex(df_im.loc[par, tp].index, fill_value=0)
-    updated_curr = df_im.loc[par, tp] + part_curr
-    df_im.loc[par, tp] = updated_curr.values.astype(np.float32)  # update original dataframe of the membrane currents
+    part_curr = part_curr.reindex(im.loc[par, tp].index, fill_value=0)
+    updated_curr = im.loc[par, tp] + part_curr
+    im.loc[par, tp] = updated_curr.values.astype(np.float32)  # update original dataframe of the membrane currents
 
 
 def get_part_out(iax_tp: float, im_tp_out: Series) -> Series:
+    """
+    Partitions a positive axial current into outward membrane currents.
+
+    Parameters:
+        iax_tp (float): The axial current value to be partitioned (must be positive).
+        im_tp_out (pd.Series): A pandas Series containing outward membrane currents (all values >= 0).
+
+    Returns:
+        pd.Series: A pandas Series containing the partitioned axial current components.
+    """
     assert (im_tp_out >= 0).all()
     ratios = im_tp_out / im_tp_out.sum()
     partitioned_out = pd.Series(ratios * iax_tp, index=im_tp_out.index)
@@ -29,6 +85,16 @@ def get_part_out(iax_tp: float, im_tp_out: Series) -> Series:
 
 
 def get_part_in(iax_tp: float, im_tp_in: Series) -> Series:
+    """
+    Partitions a negative axial current into inward membrane currents.
+
+    Parameters:
+        iax_tp (float): The axial current value to be partitioned (must be negative).
+        im_tp_in (pd.Series): A pandas Series containing inward membrane currents (all values < 0).
+
+    Returns:
+        pd.Series: A pandas Series containing the partitioned axial current components
+    """
     assert (im_tp_in < 0).all()
     ratios = im_tp_in / im_tp_in.sum()
     partitioned_in = pd.Series(ratios * iax_tp, index=im_tp_in.index)
